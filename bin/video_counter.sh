750051e4eda4ea37a24b22e5ee627578704917fa
abraxas678 <abraxas678@gmail.com>
2022-03-27 13:15:54 +0200

    1

--- bin/video_counter.sh
+++ bin/video_counter.sh
@@ -6,3 +6,9 @@ fdfind -e mp4 | wc -l
 echo
 echo "nfo:"
 fdfind -e nfo | wc -l
+echo "fanart:"
+fdfind "*fanart*" | wc -l
+echo "poster:"
+fdfind "*poster*" | wc -l
+say.sh video counter finished &>/dev/null
+send.sh video counter finished &>/dev/null
