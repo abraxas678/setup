cebb4952cd1ca54d71da2442be4e41d444831a1c
abraxas678 <abraxas678@gmail.com>
2022-03-28 14:04:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 clear
-echo V2
+echo SETUP NEW
+echo "======================"
+echo "MKDIR"
 sudo mkdir /volume1
 sudo mkdir /volume2
 sudo mkdir /volume1/sec
+sudo mkdir /volume1/supersec
 sudo mkdir /volume2/docker
 sudo mkdir /volume2/docker_final
 sudo apt update && sudo apt install -y curl unzip git wget nfs-common
 sudo mount -t nfs 192.168.86.29:volume1/sec /volume1/sec -o nolock
+sudo mount -t nfs 192.168.86.29:volume1/suprsec /volume1/supersec -o nolock
 sudo mount -t nfs 192.168.86.29:volume2/docker /volume2/docker -o nolock
 sudo mount -t nfs 192.168.86.29:volume2/docker_final /volume2/docker_final -o nolock
 echo
@@ -20,16 +24,16 @@ echo
 #wget https://github.com/gitless-vcs/gitless/releases/download/v0.8.8/gl-v0.8.8-linux-x86_64.tar.gz
 #tar xf gl-v0.8.8-linux-x86_64.tar.gz
 #cp ./gl-v0.8.8*/* /usr/local/bin/gl
-#git config --global user.email abraxas678@gmail.com
-#git config --global user.name abraxas678
+git config --global user.email abraxas678@gmail.com
+git config --global user.name abraxas678
 #####################################################################################
 #echo OH MY ZSH  - execute script again ater installation of zsh
 #echo
 #sleep 4
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
 cd $HOME
-git clone https://github.com/abraxas678/setup_new.git
-cd setup_new
+git clone https://github.com/abraxas678/setup.git
+cd setup
 echo; echo $PWD; echo
 chmod +x *
 echo
@@ -47,18 +51,18 @@ echo
 echo $PWD
 sleep 3
 echo
-ip=$(hostname)
-if [[ "$ip" = "LAPTOP-HR2FJQGL" ]]; then
-  echo MOUNTING
-  mkdir /mnt/t
-  mkdir /volume1
-  mkdir /volume2
-  mount -t drvfs T: /mnt/t
-  mount -t drvfs T: /volume2
-  mount -t drvfs T: /volume1  
-fi
-sleep 5
-echo
+#ip=$(hostname)
+#if [[ "$ip" = "LAPTOP-HR2FJQGL" ]]; then
+#  echo MOUNTING
+#  mkdir /mnt/t
+#  mkdir /volume1
+#  mkdir /volume2
+#  mount -t drvfs T: /mnt/t
+#  mount -t drvfs T: /volume2
+#  mount -t drvfs T: /volume1  
+#fi
+#sleep 5
+#echo
 echo
 source rclone_secure_setup.sh
 #curl -L https://raw.githubusercontent.com/abraxas678/setup_new/master/rclone_secure_setup.sh | bash
@@ -87,18 +91,18 @@ sudo apt update && sudo apt install -y zsh fonts-powerline xz-utils wget fd-find
 ###### https://github.com/suin/git-remind
 ################## GIT REMIND
 cd $HOME; echo
-echo GIT REMIND
-sleep 3
-mkdir git-remind
-cd git-remind
-echo
+#echo GIT REMIND
+#sleep 3
+#mkdir git-remind
+#cd git-remind
+#echo
 echo $PWD
 echo
 sleep 3
-wget https://github.com/suin/git-remind/releases/download/v1.1.1/git-remind_1.1.1_Linux_x86_64.tar.gz
-tar xf git-remind_1.1.1_Linux_x86_64.tar.gz
-git config --global remind.paths '/volume2/docker_final/setup_new/*,/volume2/docker/utils/*,/root/*,/volume2/docker_final/jdownloader'
-sleep 3
+#wget https://github.com/suin/git-remind/releases/download/v1.1.1/git-remind_1.1.1_Linux_x86_64.tar.gz
+#tar xf git-remind_1.1.1_Linux_x86_64.tar.gz
+#git config --global remind.paths '/volume2/docker_final/setup_new/*,/volume2/docker/utils/*,/root/*,/volume2/docker_final/jdownloader'
+#sleep 3
 echo
 #echo
 #echo RCLONE ROOT_TAR BEGINNT
@@ -117,13 +121,13 @@ echo
 #git clone git@github.com:abraxas678/path.git
 sudo mkdir /mytmp
 cd /mytmp
-sudo git clone git@github.com:abraxas678/root.git
-rclone move /mytmp/root $HOME/ -P -v
+sudo git clone git@github.com:abraxas678/dotfiles.git
+rclone move /mytmp/dotfiles $HOME/ -P -v
 cd $HOME
-rm -rf setup_new
-rm start.sh
-rm git-remind*tar.gz*
-rm gl*tar.gz*
-cd $HOME
+rm -rf /mytmp
+#rm start.sh
+#rm git-remind*tar.gz*
+#rm gl*tar.gz*
+#cd $HOME
 #exec zsh
 
