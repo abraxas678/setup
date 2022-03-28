aa29380568615b863544ac803c9d087c833d39d1
abraxas678 <abraxas678@gmail.com>
2022-03-28 06:31:10 +0200

    Add files via upload

---
+++ setup_ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#### SSH
+mkdir ~/.ssh
+rclone copy snas:sec/sshkeys ~/.ssh --include="id*" --include="me2" -P
+sudo chmod 400 ~/.ssh/* -R
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_rsa
+#rclone copy snas:sec . --include="me2*" -P
+#mkdir /volume1
+#mkdir /volume1/sec
+#rclone copy snas:sec /volume1/sec --include rc --max-depth 1
+#ssh-add ~/.ssh/me2
