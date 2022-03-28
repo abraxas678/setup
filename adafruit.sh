ca28f4c671de776cd80c0548cb20ce74670881d6
abraxas678 <abraxas678@gmail.com>
2022-03-28 18:05:26 +0200

    Add files via upload

---
+++ adafruit.sh
@@ -0,0 +1,4 @@
+export IO_USERNAME="abraxas678"
+export IO_KEY="aio_vMsD53a3xuoR2Z5Ta2Hx9UR5T8Sr"
+curl --silent -H "X-AIO-Key: aio_vMsD53a3xuoR2Z5Ta2Hx9UR5T8Sr" \
+  https://io.adafruit.com/api/v2/abraxas678/feeds
