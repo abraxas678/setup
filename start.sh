c1f81e2f7eafc86b7105a6697b4c46a5665a691b
abraxas678 <abraxas678@gmail.com>
2022-04-16 04:49:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo
 echo GESTARTET MIT X11 UNTERSTÜTZUNG?
-echo BUTTON
+echo
+echo "<<<BUTTON>>>"
 echo
 read me
-sudo apt update && sudo apt install git curl wget nano -y
+sudo apt update && sudo apt install git wget -y
 cd $HOME
 rm -rf $HOME/setup
 git clone https://github.com/abraxas678/setup.git
 cd $HOME/setup
+chmod +x *.sh
 ./setup.sh
