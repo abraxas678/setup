550160a3ab5ff0dc9977463c885d8c85503c3f9d
abraxas678 <abraxas678@gmail.com>
2022-04-16 04:52:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,29 @@ echo
 echo "<<<BUTTON>>>"
 echo
 read me
-sudo apt update && sudo apt install git wget -y
+echo "APT UPDATE && UPGRADE"
+echo
+sleep 1
+echo; echo
+echo "sudo apt update && sudo apt upgrade -y"
+echo
+sudo apt update && sudo apt upgrade -y
+echo
+echo "INSTALL GIT WGET"
+echo
+sleep 1
+sudo apt install git wget -y
 cd $HOME
 rm -rf $HOME/setup
+echo
+echo "CLONE setup.git"
+echo
+sleep 1
 git clone https://github.com/abraxas678/setup.git
 cd $HOME/setup
 chmod +x *.sh
+echo
+echo "SETUP.SH"
+echo
+sleep 1
 ./setup.sh
